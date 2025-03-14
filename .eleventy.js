2d58da6d6cb5523825ca15eea9541e9cd4c9b48e
Evelyn Evans <evelynjane.evans@gmail.com>
2025-03-14 10:15:01 -0500

    added list and markdown-it-task-checkbox plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,38 @@
-const pluginTOC = require('eleventy-plugin-toc')
+const markdownItCheckbox = require("markdown-it-task-checkbox")
+
+var md = require('markdown-it')()
+            .use(require('markdown-it-task-checkbox'),{
+              disabled: true,
+              divWrap: false,
+              divClass: 'checkbox',
+              idPrefix: 'cbx_',
+              ulClass: 'task-list',
+              liClass: 'task-list-item'
+            });
+
+md.render('- [x] unchecked') // =>
+// <ul class="task-list">
+//  <li class="task-list-item">
+//     <div classname="checkbox">
+//       <input type="checkbox" id="cbx_0" checked="true" disabled="true">
+//       <label for="cbx_0">unchecked</label>
+//     </div>
+//  </li>
+// </ul>
 
 module.exports = async function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("./src/css");
-    eleventyConfig.addWatchTarget("./src/css/");
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addWatchTarget("./src/css/");
 
-    eleventyConfig.addPassthroughCopy("./src/img");
-    eleventyConfig.addWatchTarget("./src/img/");
+  eleventyConfig.addPassthroughCopy("./src/img");
+  eleventyConfig.addWatchTarget("./src/img/");
 
-    return {
-      dir: {
-        input: "src",
-        output: "public",
-      },
-    };
+  eleventyConfig.amendLibrary("md", (mdLib) => mdLib.use(markdownItCheckbox));
+
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
   };
-
-  
-
+};
---
+++ src/list.md
@@ -0,0 +1,10 @@
+---
+title: List
+layout: base.njk
+---
+
+### Site Todo's
+- [ ] incomplete task
+- [x] completed task
+- [ ] Add dates to blogposts
+- [ ] Add accurate dates to blogposts
